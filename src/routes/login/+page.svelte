22cc988aecb1b9d67fb9c1000a16ee47b08bfe58
jaqb8 <qba.anisz@gmail.com>
2023-01-03 14:56:26 +0100

    test

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -28,7 +28,7 @@
 		<div class="text-center lg:text-left">
 			<h1 class="text-5xl font-bold">Login</h1>
 			<p class="py-6">
-				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+				Provifdent cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
 				quasi. In deleniti eaque aut repudiandae et a id nisi.
 			</p>
 		</div>
