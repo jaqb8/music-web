cd72538997ec7928593329d9d6ca87868b6545bb
jaqb8 <qba.anisz@gmail.com>
2023-01-30 07:58:58 +0100

    Add redis caching

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,40 @@
+import { redis } from '$lib/server/redis';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ locals }) => {
-	const result = await locals.spotifyClient.getNewReleases();
-	return result.body;
+const CACHE_KEY = 'new-releases';
+
+export const load: PageServerLoad = async ({ locals, setHeaders }) => {
+	const getNewReleases = async (): Promise<
+		SpotifyApi.PagingObject<SpotifyApi.AlbumObjectSimplified>
+	> => {
+		const cached = await redis.get(CACHE_KEY);
+
+		if (cached) {
+			console.log(`Cache ${CACHE_KEY} hit!`);
+			const ttl = await redis.ttl(CACHE_KEY);
+			setHeaders({ 'cache-control': `max-age=${ttl}` });
+			return JSON.parse(cached);
+		}
+
+		console.log(`Cache ${CACHE_KEY} miss!`);
+
+		const {
+			body: { albums },
+			headers
+		} = await locals.spotifyClient.getNewReleases();
+
+		const cacheControl = headers['cache-control'];
+		if (cacheControl) {
+			setHeaders({
+				'cache-control': cacheControl
+			});
+		}
+
+		redis.set(CACHE_KEY, JSON.stringify(albums), 'EX', 600);
+		return albums;
+	};
+
+	return {
+		albums: getNewReleases()
+	};
 };
